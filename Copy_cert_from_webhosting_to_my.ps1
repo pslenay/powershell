0c56987fe50e9da72e1fd507fab39d28b03d1af9
Aleksandr Mamin <pslenay@gmail.com>
2022-03-04 15:09:40 +0300

    Add a few PowerShell scripts

---
+++ Copy_cert_from_webhosting_to_my.ps1
@@ -0,0 +1,34 @@
+﻿$SourceStoreScope = 'LocalMachine'
+$SourceStorename = 'WebHosting'
+ 
+$SourceStore = New-Object  -TypeName System.Security.Cryptography.X509Certificates.X509Store  -ArgumentList $SourceStorename, $SourceStoreScope
+$SourceStore.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadOnly)
+ 
+$cert = $SourceStore.Certificates | Where-Object { $_.subject -eq 'CN=autodiscover.sev-zap.ru' }
+  
+
+if(Get-ChildItem -Path Cert:\LocalMachine\My -Recurse | Where-Object {$_.thumbprint -eq $Cert.Thumbprint}) {
+$SourceStore.Close()
+$DestStore.Close()}
+else {
+$DestStoreScope = 'LocalMachine'
+$DestStoreName = 'My'
+ 
+$DestStore = New-Object  -TypeName System.Security.Cryptography.X509Certificates.X509Store  -ArgumentList $DestStoreName, $DestStoreScope
+$DestStore.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
+$DestStore.Add($cert)
+
+$SourceStore.Close()
+$DestStore.Close()
+
+$Sender = "CertNotification@sev-zap.ru"
+$Recipient = "Support@sev-zap.ru"
+$Subject = 'Внимание! На сервере Exchange был обновлен сертификат Lets encrypt'
+$BodyTxt1 = 'На SPB-EX2013 был автоматически обновлен и перезаписан сертификат Lets encrypt в хранилище сертификатов "Размещение веб-служб" локального компьютера.'
+$BodyTxt2 = 'Скрипт добавил сертификат в папку "Личное" локального компьютера. Срок действия до (формат ММ:ДД:ГГ):'
+$BodyTxt3 = 'Необходимо удалить старый сертификат Lets encrypt из папки "Личное" локального компьютера.'
+$BodyTxt4 = 'Данное сообщение отправленно скриптом с сервера SPB-EX2013'
+$smtpserver ="m.sev-zap.ru"
+
+$EmailBody = $BodyTxt1 + [System.Environment]::NewLine + $BodyTxt2 + " " + ($cert).NotAfter + " " + [System.Environment]::NewLine + [System.Environment]::NewLine +$BodyTxt3 + [System.Environment]::NewLine + $BodyTxt4 -join ' '
+Send-MailMessage -To $Recipient -From $Sender -SmtpServer $smtpserver -Subject $Subject -Body $EmailBody -Encoding 'UTF8'}
---
+++ Send_mail_expiration_password.ps1
@@ -0,0 +1,30 @@
+﻿$Sender = "Кому отправить"
+$Subject = 'Внимание! Скоро истекает срок действия Вашего пароля!'
+$BodyTxt1 = 'Срок действия Вашего пароля для'
+$BodyTxt2 = 'заканчивается через '
+$BodyTxt3 = 'дней. Не забудьте заранее сменить Ваш пароль (Инструкция по смене пароля во вложении).'
+$BodyTxt4 = 'В случае возникновения вопросов, обратитесь в службу "!!Техническая поддержка".'
+$Attachment = "Локальный путь до файла"
+$smtpserver ="Имя SMTP сервера"
+$warnDays = (get-date).adddays(7)
+$2Day = get-date
+
+# Поиск пользователей с истекающим паролем (7/3/менее одного дня осталось до истечения) и отправка ему сообщения с информацией по смене пароля
+
+$UXUsers = Get-ADUser -SearchBase 'OU=Отделы,DC=Domen,DC=RU' -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} -Properties msDS-UserPasswordExpiryTimeComputed, EmailAddress, Name | select Name, @{Name ="ExpirationDate";Expression= {[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}}, EmailAddress
+foreach ($user in $UXUsers) {
+if (($user.ExpirationDate -lt (get-date).adddays(8)) -and ($user.ExpirationDate -gt (get-date).adddays(6))) {
+$lastdays = ( $user.ExpirationDate -$2Day).days
+$EmailBody = $BodyTxt1, $user.name, $BodyTxt2, $lastdays, $BodyTxt3 + [System.Environment]::NewLine + $BodyTxt4  -join ' '
+Send-MailMessage -To $user.EmailAddress -From $Sender -SmtpServer $smtpserver -Subject $Subject -Body $EmailBody -Attachments $Attachment -Encoding 'UTF8'}
+
+if (($user.ExpirationDate -lt (get-date).adddays(4)) -and ($user.ExpirationDate -gt (get-date).adddays(2))) {
+$lastdays = ( $user.ExpirationDate -$2Day).days
+$EmailBody = $BodyTxt1, $user.name, $BodyTxt2, $lastdays, $BodyTxt3 + [System.Environment]::NewLine + $BodyTxt4  -join ' '
+Send-MailMessage -To $user.EmailAddress -From $Sender -SmtpServer $smtpserver -Subject $Subject -Body $EmailBody -Attachments $Attachment -Encoding 'UTF8'}
+
+if (($user.ExpirationDate -lt (get-date).adddays(2)) -and ($user.ExpirationDate -gt (get-date))) {
+$lastdays = ( $user.ExpirationDate -$2Day).days
+$EmailBody = $BodyTxt1, $user.name, $BodyTxt2, $lastdays, $BodyTxt3 + [System.Environment]::NewLine + $BodyTxt4  -join ' '
+Send-MailMessage -To $user.EmailAddress -From $Sender -SmtpServer $smtpserver -Subject $Subject -Body $EmailBody -Attachments $Attachment -Encoding 'UTF8'}
+}
