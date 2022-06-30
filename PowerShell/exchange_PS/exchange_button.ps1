1ecf6b9ed0467d6acba995e031c3bfdbaa1d7cae
pslenay <54467143+pslenay@users.noreply.github.com>
2022-06-30 11:53:29 +0300

    Add files via upload

---
+++ PowerShell/exchange_PS/exchange_button.ps1
@@ -0,0 +1,10 @@
+﻿ $psISE.CurrentPowerShellTab.AddOnsMenu.SubMenus.Add(
+  "Connect to Exchange",
+    {
+        $s = New-PSSession -ConfigurationName Microsoft.Exchange `
+        -ConnectionUri http://d01EXCAS01.main.russianpost.ru/PowerShell/ `
+        -Authentication Kerberos
+        Import-PSSession $s
+    },
+  "Control+Alt+Z"
+)  
---
+++ PowerShell/exchange_PS/remove_mail_over_3_mounth.ps1
@@ -0,0 +1,3 @@
+﻿
+import-csv -Path "D:\scripts\Green_jaba.csv" | ForEach-Object -Process {
+Set-Mailbox -Identity $_.san -RetentionPolicy "Удаление старше 3 месяцев" }
